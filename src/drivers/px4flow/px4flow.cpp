506c0880a12f6ef6cd4625c24152152be9f0a615
Kabir Mohammed <mhkabir98@gmail.com>
2015-10-28 21:27:30 +0530

    px4flow : rotate gyro readings as well

--- src/drivers/px4flow/px4flow.cpp
+++ src/drivers/px4flow/px4flow.cpp
@@ -530,6 +530,7 @@ PX4FLOW::collect()
 	float zeroval = 0.0f;
 
 	rotate_3f(_sensor_rotation, report.pixel_flow_x_integral, report.pixel_flow_y_integral, zeroval);
+	rotate_3f(_sensor_rotation, report.gyro_x_rate_integral, report.gyro_y_rate_integral, report.gyro_z_rate_integral);
 
 	if (_px4flow_topic == nullptr) {
 		_px4flow_topic = orb_advertise(ORB_ID(optical_flow), &report);
