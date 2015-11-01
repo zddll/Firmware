35405c650b1f5d8b9b9ef51afcbb8f93f57ac467
Kabir Mohammed <mhkabir98@gmail.com>
2015-11-01 13:50:18 +0530

    sdlog2: add logging for filtered flow message

--- src/modules/sdlog2/sdlog2_messages.h
+++ src/modules/sdlog2/sdlog2_messages.h
@@ -479,6 +479,8 @@ struct log_MACS_s {
 	float yaw_rate_integ;
 };
 
+/* WARNING: ID 46 is already in use for ATTC1 */
+
 /* --- CONTROL STATE --- */
 #define LOG_CTS_MSG 47
 struct log_CTS_s {
@@ -491,7 +493,20 @@ struct log_CTS_s {
 	float yaw_rate;
 };
 
-/* WARNING: ID 46 is already in use for ATTC1 */
+/* --- FFLW - FILTERED OPTICAL FLOW STATUS */
+#define LOG_FFLW_MSG 48
+struct log_FFLW_s {
+	float x_vel_integ;
+	float y_vel_integ;
+	float x_vel;
+	float y_vel;
+	float gyro_x_rate;
+	float gyro_y_rate;
+	float gyro_z_rate;
+	float gyro_x_bias;
+	float gyro_y_bias;
+	float gyro_z_bias;
+};
 
 /********** SYSTEM MESSAGES, ID > 0x80 **********/
 
@@ -565,6 +580,7 @@ static const struct log_format_s log_formats[] = {
 	LOG_FORMAT(ENCD, "qfqf",	"cnt0,vel0,cnt1,vel1"),
 	LOG_FORMAT(TSYN, "Q", 		"TimeOffset"),
 	LOG_FORMAT(MACS, "fff", "RRint,PRint,YRint"),
+	LOG_FORMAT(FFLW, "ffffffffff", "XIntg,YIntg,VelX,VelY,RateX,RateY,RateZ,BiasX,BiasY,BiasZ"),
 
 	/* system-level messages, ID >= 0x80 */
 	/* FMT: don't write format of format message, it's useless */
